9a6932b3214de2c4fb2da767c544e36069fc8b1d
II-Paulus-II <paulus.coding@gmail.com>
2024-03-30 00:03:14 +0000

    should have fixed the bug where enter and delete can get pressed at the same time

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -45,3 +45,23 @@ export const inititalStateKeyboard = [
   [{letter: "A", class: "default" },{letter: "S", class: "default" },{letter: "D", class: "default" },{letter: "F", class: "default" },{letter: "G", class: "default" },{letter: "H", class: "default" },{letter: "J", class: "default" },{letter: "K", class: "default" },{letter: "L", class: "default" }],
   [{letter: "Enter", class: "enter-button" },{letter: "Z", class: "default" },{letter: "X", class: "default" },{letter: "C", class: "default" },{letter: "V", class: "default" },{letter: "B", class: "default" },{letter: "N", class: "default" },{letter: "M", class: "default" },{letter: "Delete", class: "del-button" }],
 ];
+
+export const deleteAllowed = {
+  _value: true,
+  get value() {
+    return this._value;
+  },
+  set value(arg) {
+    this._value = arg;
+  }
+};
+
+export const enterAllowed = {
+  _value: true,
+  get value() {
+    return this._value;
+  },
+  set value(arg) {
+    this._value = arg;
+  }
+};
